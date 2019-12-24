b382ab674b589fc8c849d303ac55adbf181176a4
Shoham Peller <shohamp@gmail.com>
2019-12-24 17:47:19 +0200

    Added context menu to tree view

--- widgets/region_tree.py
+++ widgets/region_tree.py
@@ -3,14 +3,22 @@ from task_manager import TaskManager
 from tasks.list_vaults import ListVaultsTask
 
 from PyQt5.QtGui import QStandardItemModel, QStandardItem
-from PyQt5.QtWidgets import QTreeView, QMainWindow
+from PyQt5.QtWidgets import QTreeView, QMainWindow, QAction
 
 from consts import REGIONS
 
 
-class RegionTree(object):
-    def __init__(self, window: QMainWindow):
+class _RegionTree(object):
+    def __init__(self):
+        self.view = None
+        self.model = None
+
+    def initialize(self, window: QMainWindow):
         self.view: QTreeView = cast(QTreeView, window.findChild(QTreeView, 'treeView'))
+
+        get_inventory_action = QAction("asdf", self.view)
+        get_inventory_action.triggered.connect(self.get_inventory_action)
+        self.view.addAction(get_inventory_action)
 
         self.model = QStandardItemModel()
         root: QStandardItem = self.model.invisibleRootItem()
@@ -21,8 +29,8 @@ class RegionTree(object):
             root.appendRow(item)
             TaskManager.add_task(ListVaultsTask(region, self.view))
 
-        self.view.setModel(self.model)
+    def get_inventory_action(self):
+        print(self.view.selectedIndexes()[0].data())
 
-    @classmethod
-    def initialize(cls, window: QMainWindow):
-        cls(window)
+
+RegionTree = _RegionTree()
