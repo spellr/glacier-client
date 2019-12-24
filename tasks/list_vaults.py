4d2bce5ecfe131f14fee664403d1689413b05e63
Shoham Peller <shohamp@gmail.com>
2019-12-24 21:07:30 +0200

    Fixed context menu to show different actions on regions and vaults
    And move view logic to view class from task

--- tasks/list_vaults.py
+++ tasks/list_vaults.py
@@ -4,17 +4,12 @@ from consts import Region
 from tasks.base_task import Task
 from keys import PUBLIC_KEY, SECRET_KEY
 
-from PyQt5.QtWidgets import QTreeView
-from PyQt5.QtCore import QModelIndex
-from PyQt5.QtGui import QStandardItem
-
 
 class ListVaultsTask(Task):
-    def __init__(self, region: Region, view: QTreeView):
+    def __init__(self, region: Region, tree_view):
         super(ListVaultsTask, self).__init__()
         self.region = region
-        self.view = view
-        self.model = self.view.model()
+        self.tree_view = tree_view
 
     def __repr__(self):
         return f"{self.__class__.__name__}({self.region})"
@@ -27,24 +22,6 @@ class ListVaultsTask(Task):
         for vault in vaults:
             print(f"{self.region.name}: {vault['VaultName']}")
 
-        root: QModelIndex = self.view.rootIndex()
-        child_count = self.model.rowCount(root)
-        parent = QModelIndex()
-
-        for i in range(child_count):
-            row = self.model.index(i, 0, parent)
-            cur_region = row.data()
-            if self.region.name == cur_region:
-                break
-        else:
-            raise ValueError(f"Failed to find region {self.region}")
-
         for vault in vaults:
             vault_name = vault['VaultName']
-            item = QStandardItem(vault_name)
-            self.model.itemFromIndex(row).appendRow(item)
-
-        self.view.expandAll()
-
-        # Refresh model to see new rows
-        self.model.rowsInserted.emit(parent, 0, child_count)
+            self.tree_view.add_vault(self.region, vault_name)
--- widgets/region_tree.py
+++ widgets/region_tree.py
@@ -1,9 +1,12 @@
 from typing import cast
+
+from PyQt5.QtCore import QModelIndex
+
 from task_manager import TaskManager
 from tasks.list_vaults import ListVaultsTask
 
 from PyQt5.QtGui import QStandardItemModel, QStandardItem
-from PyQt5.QtWidgets import QTreeView, QMainWindow, QAction
+from PyQt5.QtWidgets import QTreeView, QMainWindow, QAction, QMenu
 
 from consts import REGIONS
 
@@ -16,9 +19,7 @@ class _RegionTree(object):
     def initialize(self, window: QMainWindow):
         self.view: QTreeView = cast(QTreeView, window.findChild(QTreeView, 'treeView'))
 
-        get_inventory_action = QAction("asdf", self.view)
-        get_inventory_action.triggered.connect(self.get_inventory_action)
-        self.view.addAction(get_inventory_action)
+        self.view.customContextMenuRequested.connect(self.open_menu)
 
         self.model = QStandardItemModel()
         root: QStandardItem = self.model.invisibleRootItem()
@@ -27,7 +28,50 @@ class _RegionTree(object):
         for region in REGIONS:
             item = QStandardItem(region.name)
             root.appendRow(item)
-            TaskManager.add_task(ListVaultsTask(region, self.view))
+            TaskManager.add_task(ListVaultsTask(region, self))
+
+    def open_menu(self, position):
+        level = 0
+
+        indexes = self.view.selectedIndexes()
+        if len(indexes) > 0:
+            index = indexes[0]
+            while index.parent().isValid():
+                index = index.parent()
+                level += 1
+
+        menu = QMenu()
+        if level == 0:
+            get_all_inventories_action = QAction("Request last inventory for all vaults", self.view)
+            get_all_inventories_action.triggered.connect(self.get_inventory_action)
+            menu.addAction(get_all_inventories_action)
+        else:
+            get_inventory_action = QAction("Request last inventory", self.view)
+            get_inventory_action.triggered.connect(self.get_inventory_action)
+            menu.addAction(get_inventory_action)
+
+        menu.exec(self.view.viewport().mapToGlobal(position))
+
+    def add_vault(self, region, vault_name):
+        root: QModelIndex = self.view.rootIndex()
+        child_count = self.model.rowCount(root)
+        parent = QModelIndex()
+
+        for i in range(child_count):
+            row = self.model.index(i, 0, parent)
+            cur_region = row.data()
+            if region.name == cur_region:
+                break
+        else:
+            raise ValueError(f"Failed to find region {region}")
+
+        item = QStandardItem(vault_name)
+        self.model.itemFromIndex(row).appendRow(item)
+
+        self.view.expandAll()
+
+        # Refresh model to see new rows
+        self.model.rowsInserted.emit(parent, 0, child_count)
 
     def get_inventory_action(self):
         print(self.view.selectedIndexes()[0].data())
