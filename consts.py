13ee6d971c8b80debc2463b2dd605db9f8b4fbdc
Shoham Peller <shohamp@gmail.com>
2020-01-01 14:50:15 +0200

    Added caching of inventories

--- consts.py
+++ consts.py
@@ -1,1 +1,10 @@
+import os
+import appdirs
+
 DEBUG = True
+
+APP_NAME = "glacier_client"
+
+DATA_DIR = appdirs.user_data_dir(APP_NAME)
+if not os.path.exists(DATA_DIR):
+    os.mkdir(DATA_DIR)
--- inventory_manager.py
+++ inventory_manager.py
@@ -1,19 +1,39 @@
+import os
+import pickle
 from typing import Dict, Sequence
 from collections import defaultdict
 
+from consts import DATA_DIR
 from archive import Archive
 from regions import Region
 
 
 class _Inventories(object):
+    INVENTORY_CACHE_FILE = os.path.join(DATA_DIR, "inventories.pickle")
+
     def __init__(self):
-        self.inventories: Dict[str, Sequence[Archive]] = defaultdict(lambda: [])
+        self.inventories: Dict[str, Sequence[Archive]]
+        if os.path.exists(self.INVENTORY_CACHE_FILE):
+            try:
+                self.inventories = pickle.load(open(self.INVENTORY_CACHE_FILE, 'rb'))
+            except EOFError:
+                self.inventories = {}
+        else:
+            self.inventories = {}
+
+    def _dump_to_cache(self):
+        pickle.dump(self.inventories, open(self.INVENTORY_CACHE_FILE, 'wb'))
 
     def new_inventory(self, region: Region, vault: str, inventory: Sequence[Archive]):
         self.inventories[region.code + vault] = inventory
+        self._dump_to_cache()
 
     def get_inventory(self, region: Region, vault: str):
-        return self.inventories[region.code + vault]
+        key = region.code + vault
+        if key in self.inventories:
+            return self.inventories[key]
+        else:
+            return []
 
 
 Inventories = _Inventories()
