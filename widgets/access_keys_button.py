f029f029ba0376cb2f1f5a576e3263a44253993e
Shoham Peller <shohamp@gmail.com>
2020-01-02 15:28:21 +0200

    Removed help button from dialog

--- widgets/access_keys_button.py
+++ widgets/access_keys_button.py
@@ -2,7 +2,8 @@ import logging
 from typing import Optional, cast
 
 from PyQt5 import uic
-from PyQt5.QtCore import QObject
+from PyQt5.QtCore import QObject, Qt
+from PyQt5.QtGui import QCloseEvent, QHideEvent
 from PyQt5.QtWidgets import QPushButton, QMainWindow, QDialog
 
 from widgets import widgets_map
@@ -13,6 +14,7 @@ access_keys_dialog = uic.loadUiType("keys_dialog.ui")[0]
 class AccessKeysDialog(QDialog, access_keys_dialog):
     def __init__(self, parent=None):
         QDialog.__init__(self, parent)
+        self.setWindowFlag(Qt.WindowContextHelpButtonHint, False)
         self.setupUi(self)
 
 
